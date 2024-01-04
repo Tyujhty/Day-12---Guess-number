eeecdf572c21bf55624d0d06c4bbd59708dff096
Tyujhty <fannybaquey@gmail.com>
2024-01-04 22:41:45 +0000

    feat: compare function that return the result. Logic play again function.

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-#Number Guessing Game Objectives:
-
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 import random
+from replit import clear 
 from art import logo
-
-print(logo)
-print("Welcome, try to guess the computer choice between 1 and 100.\nTwo levels of difficulties.\n    Good luck :)")
 
 def computer_choice():
     '''Choose a random number'''
@@ -19,17 +9,56 @@ def computer_choice():
 def choose_difficulties():
     number_lives = 0
     user_choice = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-    
+    '''Determinate number of lives'''
     if user_choice == 'easy':
         number_lives = 10
     elif user_choice == 'hard':
         number_lives = 5
-
+    
+    print(f"You have {number_lives} attempts remainings to guess the number.")
+    
     return number_lives
 
+def compare_results(random_number, number_lives):
+    while number_lives != 0:
+        
+        user_guess = int(input("Make a guess: "))
+        print(user_guess)
+        result = ''
+        if user_guess < random_number:
+            result = "Too low."
+        elif user_guess > random_number:
+            result = 'Tow high.'
+        else:
+            play_again = input("Congratulations, you guess the number\nTry again? 'y' or 'n': ")
+            if play_again == 'y':
+                return True
+            else:
+                return False
+            
+        number_lives -= 1
+        
+        print(result)
+        print(f"You have {number_lives} attempts remainings to guess the number.\nGuess again!")
+    
+    return False
 
-random_number = computer_choice()
-print(random_number)
+def game():
+    start = True
+    while start is not False:
+        clear()
+        print(logo)
+        print("Welcome, try to guess the computer choice between 1 and 100.\nTwo levels of difficulties.\n    Good luck :)")
+        
+        random_number = computer_choice()
 
-number_lives = choose_difficulties()
-print(number_lives)
+        number_lives = choose_difficulties()
+
+        play_again = compare_results(random_number, number_lives)
+
+        if not play_again:
+            replay = input(f"Sorry, you didn't find the {random_number}.\nTry again? 'y' or 'n'").lower()
+            if replay != 'y':
+                print("Good bye :(")
+                start = False
+game()
